47e573e92ddd9a72563cbeff7ab88a84af105bda
davantio <davantiodestama07@gmail.com>
2022-11-29 19:59:52 +0700

    Membuat BoM

--- resources/views/bom/bom.blade.php
+++ resources/views/bom/bom.blade.php
@@ -8,7 +8,7 @@
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="/home">Home</a></li>
-          <li class="breadcrumb-item">BoM<</li>
+          <li class="breadcrumb-item">BoM</li>
           <li class="breadcrumb-item active">Data BoM</li>
         </ol>
       </nav>
